5dcaf4abd22f6b7eb17eccb36b0de88764c1b5f8
Jon River <jon.cc.cheng@gmail.com>
2019-09-12 09:58:24 +0800

    Add Student Class

---
+++ app/src/main/java/com/jon/example/dagger2exercise/Student.java
@@ -0,0 +1,17 @@
+package com.jon.example.dagger2exercise;
+
+public class Student {
+    private String name = "None";
+
+    public Student() {
+    }
+
+    public void setName(String str) {
+        name = str;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+}
